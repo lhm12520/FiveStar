bba4390a1c56ac0dcf7f83ce46d6ecf6d93bd9ab
刘宏敏 <liuhongmin@liuhongmindeMacBook-Pro.local>
2014-03-21 11:12:28 +0800

    Initial Commit

---
+++ FiveStar/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  FiveStar
+//
+//  Created by 刘宏敏 on 14-3-21.
+//  Copyright (c) 2014年 刘宏敏. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
